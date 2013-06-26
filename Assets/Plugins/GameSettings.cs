b77125d195e223dc9e91bc33efa513313d58fef4
SamkyBoy <samuel.boullier@gmail.com>
2013-06-26 17:03:21 +0200

    CLEAN FOLDER

---
+++ Assets/Plugins/GameSettings.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameSettings : MonoBehaviour
+{
+    public static void Clear()
+    {
+        serverTitle = description = motd = IP = password = "";
+        isDedicatedServer = false;
+    }
+
+
+    //MP server specific
+    public static string serverTitle;
+    public static string description;
+    public static string motd;
+    public static string password;
+    public static string IP;
+    public static int port;
+    public static int players;
+    public static bool isDedicatedServer;
+
+
+}
