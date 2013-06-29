4fa8b44e9c83ef3cbb254c56f2b3f21f4bbdfeb9
Otakode <marchal.cl@gmail.com>
2013-06-29 11:57:22 +0200

    fire weapon with target
    Signed-off-by: Otakode <marchal.cl@gmail.com>

--- Assets/Scripts/OculusMode.cs
+++ Assets/Scripts/OculusMode.cs
@@ -6,7 +6,7 @@ public delegate void ChangeOculusMode();
 public class OculusMode : MonoBehaviour
 {
 	public static ChangeOculusMode changeOculusMode;
-	private static bool _on = false;
+	private static bool _on = true;
 	public static bool on
 	{
 		get
